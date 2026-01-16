2459ff206259dddcac846bd632db5c8e529865b5
Eltaf-azizi <iltfhuss@gmail.com>
2026-01-16 22:58:33 +0500

    working on the firebase options

--- driver_app/lib/main.dart
+++ driver_app/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
-import '../../../firebase_options.dart'; // You'll need to generate this
+import 'firebase_options.dart'; // You'll need to generate this
 import '../../../screens/home_screen.dart';
 import '../../../screens/login_screen.dart';
 import '../../../services/auth_service.dart';
