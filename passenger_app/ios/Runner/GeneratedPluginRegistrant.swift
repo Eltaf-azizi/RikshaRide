e9c215845845ee2b811e30a5fb7dd82b88e62701
Eltaf-azizi <iltfhuss@gmail.com>
2026-01-31 23:19:17 +0500

    working on the runner

---
+++ passenger_app/ios/Runner/GeneratedPluginRegistrant.swift
@@ -0,0 +1,12 @@
+//
+// Generated file. Do not edit.
+//
+
+import FlutterMacOS
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FirebaseAuthPlugin"))
+  FirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FirebaseCorePlugin"))
+  CloudFirestorePlugin.register(with: registry.registrar(forPlugin: "CloudFirestorePlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
+}
